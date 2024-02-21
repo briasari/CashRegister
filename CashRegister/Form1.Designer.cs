eeb290c05c8262d225c3cc4dd14ea1d8759c055b
briasari <briasari660@gmail.com>
2024-02-21 13:52:57 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pattyLabel = new System.Windows.Forms.Label();
+            this.baconLabel = new System.Windows.Forms.Label();
+            this.lettuceLabel = new System.Windows.Forms.Label();
+            this.cheeseLabel = new System.Windows.Forms.Label();
+            this.tomatoLabel = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.tenderedButton = new System.Windows.Forms.Button();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pattyInput = new System.Windows.Forms.TextBox();
+            this.baconInput = new System.Windows.Forms.TextBox();
+            this.lettuceInput = new System.Windows.Forms.TextBox();
+            this.tomatoInput = new System.Windows.Forms.TextBox();
+            this.cheeseInput = new System.Windows.Forms.TextBox();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pattyLabel
+            // 
+            this.pattyLabel.AutoSize = true;
+            this.pattyLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pattyLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pattyLabel.Location = new System.Drawing.Point(23, 6);
+            this.pattyLabel.Name = "pattyLabel";
+            this.pattyLabel.Size = new System.Drawing.Size(59, 25);
+            this.pattyLabel.TabIndex = 1;
+            this.pattyLabel.Text = "patty";
+            // 
+            // baconLabel
+            // 
+            this.baconLabel.AutoSize = true;
+            this.baconLabel.BackColor = System.Drawing.Color.Transparent;
+            this.baconLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baconLabel.Location = new System.Drawing.Point(24, 38);
+            this.baconLabel.Name = "baconLabel";
+            this.baconLabel.Size = new System.Drawing.Size(69, 25);
+            this.baconLabel.TabIndex = 2;
+            this.baconLabel.Text = "bacon";
+            // 
+            // lettuceLabel
+            // 
+            this.lettuceLabel.AutoSize = true;
+            this.lettuceLabel.BackColor = System.Drawing.Color.Transparent;
+            this.lettuceLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettuceLabel.Location = new System.Drawing.Point(24, 70);
+            this.lettuceLabel.Name = "lettuceLabel";
+            this.lettuceLabel.Size = new System.Drawing.Size(75, 25);
+            this.lettuceLabel.TabIndex = 3;
+            this.lettuceLabel.Text = "lettuce";
+            // 
+            // cheeseLabel
+            // 
+            this.cheeseLabel.AutoSize = true;
+            this.cheeseLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cheeseLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheeseLabel.Location = new System.Drawing.Point(23, 102);
+            this.cheeseLabel.Name = "cheeseLabel";
+            this.cheeseLabel.Size = new System.Drawing.Size(76, 25);
+            this.cheeseLabel.TabIndex = 4;
+            this.cheeseLabel.Text = "cheese";
+            // 
+            // tomatoLabel
+            // 
+            this.tomatoLabel.AutoSize = true;
+            this.tomatoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tomatoLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tomatoLabel.Location = new System.Drawing.Point(23, 134);
+            this.tomatoLabel.Name = "tomatoLabel";
+            this.tomatoLabel.Size = new System.Drawing.Size(79, 25);
+            this.tomatoLabel.TabIndex = 5;
+            this.tomatoLabel.Text = "tomato";
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtotalLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(23, 201);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(87, 25);
+            this.subtotalLabel.TabIndex = 6;
+            this.subtotalLabel.Text = "subtotal";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.BackColor = System.Drawing.Color.Transparent;
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(23, 233);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(40, 25);
+            this.taxLabel.TabIndex = 7;
+            this.taxLabel.Text = "tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(23, 265);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(54, 25);
+            this.totalLabel.TabIndex = 8;
+            this.totalLabel.Text = "total";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.AutoSize = true;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(87, 166);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(87, 32);
+            this.calculateButton.TabIndex = 9;
+            this.calculateButton.Text = "calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // tenderedButton
+            // 
+            this.tenderedButton.AutoSize = true;
+            this.tenderedButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedButton.Location = new System.Drawing.Point(87, 335);
+            this.tenderedButton.Name = "tenderedButton";
+            this.tenderedButton.Size = new System.Drawing.Size(87, 32);
+            this.tenderedButton.TabIndex = 10;
+            this.tenderedButton.Text = "calculate";
+            this.tenderedButton.UseVisualStyleBackColor = true;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(22, 301);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(95, 25);
+            this.tenderedLabel.TabIndex = 11;
+            this.tenderedLabel.Text = "tendered";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "cheese";
+            // 
+            // pattyInput
+            // 
+            this.pattyInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pattyInput.Location = new System.Drawing.Point(132, 5);
+            this.pattyInput.Name = "pattyInput";
+            this.pattyInput.Size = new System.Drawing.Size(111, 27);
+            this.pattyInput.TabIndex = 14;
+            // 
+            // baconInput
+            // 
+            this.baconInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baconInput.Location = new System.Drawing.Point(132, 37);
+            this.baconInput.Name = "baconInput";
+            this.baconInput.Size = new System.Drawing.Size(111, 27);
+            this.baconInput.TabIndex = 15;
+            // 
+            // lettuceInput
+            // 
+            this.lettuceInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettuceInput.Location = new System.Drawing.Point(132, 69);
+            this.lettuceInput.Name = "lettuceInput";
+            this.lettuceInput.Size = new System.Drawing.Size(111, 27);
+            this.lettuceInput.TabIndex = 16;
+            // 
+            // tomatoInput
+            // 
+            this.tomatoInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tomatoInput.Location = new System.Drawing.Point(132, 133);
+            this.tomatoInput.Name = "tomatoInput";
+            this.tomatoInput.Size = new System.Drawing.Size(111, 27);
+            this.tomatoInput.TabIndex = 18;
+            // 
+            // cheeseInput
+            // 
+            this.cheeseInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheeseInput.Location = new System.Drawing.Point(132, 101);
+            this.cheeseInput.Name = "cheeseInput";
+            this.cheeseInput.Size = new System.Drawing.Size(111, 27);
+            this.cheeseInput.TabIndex = 17;
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedInput.Location = new System.Drawing.Point(127, 301);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(116, 27);
+            this.tenderedInput.TabIndex = 22;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.totalOutput);
+            this.groupBox1.Controls.Add(this.taxOutput);
+            this.groupBox1.Controls.Add(this.subtotalOutput);
+            this.groupBox1.Controls.Add(this.pattyLabel);
+            this.groupBox1.Controls.Add(this.tenderedInput);
+            this.groupBox1.Controls.Add(this.baconLabel);
+            this.groupBox1.Controls.Add(this.lettuceLabel);
+            this.groupBox1.Controls.Add(this.tomatoInput);
+            this.groupBox1.Controls.Add(this.cheeseInput);
+            this.groupBox1.Controls.Add(this.cheeseLabel);
+            this.groupBox1.Controls.Add(this.lettuceInput);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.baconInput);
+            this.groupBox1.Controls.Add(this.tomatoLabel);
+            this.groupBox1.Controls.Add(this.pattyInput);
+            this.groupBox1.Controls.Add(this.subtotalLabel);
+            this.groupBox1.Controls.Add(this.tenderedLabel);
+            this.groupBox1.Controls.Add(this.taxLabel);
+            this.groupBox1.Controls.Add(this.tenderedButton);
+            this.groupBox1.Controls.Add(this.totalLabel);
+            this.groupBox1.Controls.Add(this.calculateButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(526, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 372);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.BackColor = System.Drawing.Color.White;
+            this.totalOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalOutput.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalOutput.Location = new System.Drawing.Point(127, 264);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(116, 25);
+            this.totalOutput.TabIndex = 25;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.BackColor = System.Drawing.Color.White;
+            this.taxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxOutput.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxOutput.Location = new System.Drawing.Point(127, 232);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(116, 25);
+            this.taxOutput.TabIndex = 24;
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.BackColor = System.Drawing.Color.White;
+            this.subtotalOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.subtotalOutput.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalOutput.Location = new System.Drawing.Point(127, 200);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(116, 25);
+            this.subtotalOutput.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(790, 378);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label pattyLabel;
+        private System.Windows.Forms.Label baconLabel;
+        private System.Windows.Forms.Label lettuceLabel;
+        private System.Windows.Forms.Label cheeseLabel;
+        private System.Windows.Forms.Label tomatoLabel;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button tenderedButton;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox pattyInput;
+        private System.Windows.Forms.TextBox baconInput;
+        private System.Windows.Forms.TextBox lettuceInput;
+        private System.Windows.Forms.TextBox tomatoInput;
+        private System.Windows.Forms.TextBox cheeseInput;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label taxOutput;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+
+        double pattyPrice = 1.11;
+        double baconPrice = 0.16;
+        double cheesePrice = 0.13;
+        double lettucePrice = 0.07;
+        double tomatoPrice = 0.02;
+        double ratPrice = 1.11;
+        int numOfPatty;
+        int numOfBacon;
+        int numOfCheese;
+        int numOfLettuce;
+        int numOfTomato;
+        int numOfRat;
+
+        double taxRate = 0.13;
+        double subtotal;
+        double taxAmount;
+        double totalPrice;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            pattyLabel.Parent = groupBox1;
+            baconLabel.Parent = groupBox1;
+            lettuceLabel.Parent = groupBox1;
+            tomatoLabel.Parent = groupBox1;
+            cheeseLabel.Parent = groupBox1;
+            calculateButton.Parent = groupBox1;
+            tenderedButton.Parent = groupBox1;
+            subtotalLabel.Parent = groupBox1;
+            totalLabel.Parent = groupBox1;
+            taxLabel.Parent = groupBox1;
+            subtotalOutput.Parent = groupBox1;
+            totalOutput.Parent = groupBox1;
+
+
+        }
+
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numOfPatty = Convert.ToInt32(pattyInput.Text);
+                numOfBacon = Convert.ToInt32(baconInput.Text);
+                numOfLettuce = Convert.ToInt32(lettuceInput.Text);
+                numOfTomato = Convert.ToInt32(tomatoInput.Text);
+                numOfCheese = Convert.ToInt32(cheeseInput.Text);
+                //numOfRat = Convert.ToInt32(ratInput.Text);
+
+
+                subtotal = pattyPrice * numOfPatty + baconPrice * numOfBacon + lettucePrice * numOfLettuce + tomatoPrice * numOfTomato + cheesePrice * numOfCheese + ratPrice * numOfRat;
+                taxAmount = taxRate * subtotal;
+                totalPrice = subtotal + taxAmount;
+
+                subtotalOutput.Text = $"{subtotal.ToString("C")}";
+                taxOutput.Text = $"{taxAmount.ToString("C")}";
+                totalOutput.Text = $"{totalPrice.ToString("C")}";
+                //.ToString(".00") to make it two decimals
+            }
+            catch
+            {
+                subtotalOutput.Text = "ERROR";
+                taxOutput.Text = string.Empty;
+                totalOutput.Text = string.Empty;
+            }
+
+        }
+
+    }
+}
